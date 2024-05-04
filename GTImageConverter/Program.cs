e5b7a1baeb39d539421a2259b6574e1bd72b54c8
Gennaro Eduardo Tangari <gennaro.tangari@outlook.com>
2024-05-04 17:28:52 +0200

    Adding the first version of GTImageConverter

--- GTImageConverter/Program.cs
+++ GTImageConverter/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImageMagick;
+
+var inputPath = @"C:\Temp\Images";  // Set the path to the folder containing the HEIC files
+var outputPath = inputPath;  // Use the same path for output files or change it if necessary
+
+var files = Directory.GetFiles(inputPath, "*.HEIC", SearchOption.AllDirectories);
+foreach (var file in files)
+{
+    try
+    {
+        using (var image = new MagickImage(file))
+        {
+            var outputFile = Path.ChangeExtension(file, ".jpg");
+            image.Write(outputFile);
+            Console.WriteLine($"Converted: {outputFile}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error processing {file}: {ex.Message}");
+    }
+}
